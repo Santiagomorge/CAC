f7251b55c8a93905b94cffc1db5517cceab8e36f
Santiago Moreno <santiago.moreno@cetus.com.co>
2024-10-23 08:15:35 -0500

    push microservicios a mi repo personal

---
+++ mic-cac-recurso/mic-cac-recurso/src/main/java/cetus/com/mic_cac_recurso/MicroservicesRecursoApplication.java
@@ -0,0 +1,13 @@
+package cetus.com.mic_cac_recurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesRecursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesRecursoApplication.class, args);
+	}
+
+}
---
+++ mic-cac-recurso/mic-cac-recurso/src/test/java/cetus/com/mic_cac_recurso/MicroservicesRecursoApplicationTests.java
@@ -0,0 +1,13 @@
+package cetus.com.mic_cac_recurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesRecursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
