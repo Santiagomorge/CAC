1d755b74bd7a394d7c03633b46faa135c48c2852
Santiago Moreno <santiago.moreno@cetus.com.co>
2024-11-06 17:58:09 -0500

    eliminación de .git mic-cac-visitantes

--- Usuario/src/main/java/cetus/com/microservices/usuario/domain/entity/Role.java
+++ Usuario/src/main/java/cetus/com/microservices/usuario/domain/entity/Role.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Role {
+public class    Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
